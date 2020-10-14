c59d896119b21087e7946ac44372cee0411fe7a0
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2020-10-13 22:48:56 -0300

    added the route that redirect to show() function in TaskController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TasksController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\TasksController;
-Route::get('user', [TasksController::class, 'index']);
+
+Route::get('tasks', [TasksController::class, 'index']);
+Route::post('register_task', [TasksController::class, 'store']);
+Route::get('task/{id}', [TasksController::class, 'show']);
