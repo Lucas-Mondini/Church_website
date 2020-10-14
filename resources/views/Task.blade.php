d7c9dc62e57e7e87508cdcf2441e85894f88e043
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2020-10-13 22:51:10 -0300

    made a view to show only one task

---
+++ resources/views/Task.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/app.css">
+        <title>Tasks</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="task">
+            <p>{{json_encode($task)}}</p>
+        </div>
+    </body>
+</html>
