1bebb5bd6d660e27537e0df7313ed994152a4500
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2020-10-14 22:22:40 -0300

    view Updated

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,7 +6,6 @@
         <link rel="stylesheet" href="css/app.css">
         <title>Tasks</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
         <style>
         </style>
     </head>
