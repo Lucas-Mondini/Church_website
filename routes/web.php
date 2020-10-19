28c391e8ed278308556512817c2f90d1e698a68e
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2020-10-19 20:32:52 -0300

    changed the name of the index() route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('tasks', [TasksController::class, 'index']);
+Route::get('list_tasks', [TasksController::class, 'index']);
 Route::post('register_task', [TasksController::class, 'store']);
 Route::get('task/{id}', [TasksController::class, 'show']);
